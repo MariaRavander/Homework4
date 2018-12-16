34d7c2a20735be0c29c538294127e0563ffadeb0
MariaRavander <maria@ravander.nu>
2018-12-16 16:31:46 +0100

    Add files via upload

---
+++ currencyConverter/src/main/java/converter/Main.java
@@ -0,0 +1,16 @@
+
+package converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starts the converter application.
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
